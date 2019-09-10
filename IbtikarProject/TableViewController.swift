581c87d4ad2ea7327ac91e67531b2b8992fcd15a
samira273 <samiramarassy@gmail.com>
2019-09-10 15:21:13 +0200

    Cancel handled first

--- IbtikarProject/TableViewController.swift
+++ IbtikarProject/TableViewController.swift
@@ -18,49 +18,93 @@ class TableViewController: UITableViewController, UISearchBarDelegate {
       var pageNo = 1
       let myRefreshControler = UIRefreshControl()
       var apiTotalPages : Int?
-      var searchActive : Bool = false
+      var searchWasDone : Bool = false
       var defaultURL = ""
       let peopleURL = "https://api.themoviedb.org/3/person/popular?api_key=6b93b25da5cdb9298216703c40a31832&language=en-US&page="
       
-      @IBOutlet weak var searchField: UISearchBar!
+      @IBOutlet weak var searchBar: UISearchBar!
       
       
       
-      func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-            
-            searchActive = true;
-      }
-      
+//      func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+//
+////            searchActive = true;
+//      }
+
       func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
-            searchActive = false;
+            //            searchActive = false;
+            searchBar.setShowsCancelButton(true, animated: false)
       }
-      
+      //
       func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-            searchActive = false;
-      }
-      
-      func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+            //            searchActive = false;
+            print("cancel was selected")
             
-            searchActive = false;
+            searchBar.resignFirstResponder()
+            searchBar.endEditing(true)
+            searchBar.text = ""
+            self.pageNo = 1
+            arrayOfPersons = []
+            
+            getData(pageNumber: 1, urlString: peopleURL)
+            
+            self.tableView.reloadData()
+      }
+
+      func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+
+//            searchActive = false;
+            print("search bar has been clicked")
+            
+            searchWasDone = true
+            let searchKey = searchBar.text
+            print(searchKey!)
+            searchBar.setShowsCancelButton(true, animated: false)
+            searchBar.resignFirstResponder()
+            if let cancelButton : UIButton = searchBar.value(forKey: "_cancelButton") as? UIButton{
+                  cancelButton.isEnabled = true
+            }
+            
+            if(searchKey != nil){
+//                  let searchUrl = "https://api.themoviedb.org/3/search/person?api_key=facd2bc8ee066628c8f78bbb7be41943&query="+searchKey!
+                  
+                  let searchUrl = "https://api.themoviedb.org/3/search/person?api_key=6b93b25da5cdb9298216703c40a31832&language=en-US&query=\(searchKey?.replacingOccurrences(of: " ", with: "%20") ?? "")&include_adult=false&page="
+                  
+                  getData(pageNumber: 1, urlString: searchUrl)
+                  tableView.reloadData()
+                  
+            }
+//            else{
+//                  getData(pageNumber: 1, urlString: peopleURL)
+//            }
       }
       
       func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
             
             //remove spaces
-            
-            var searchURL = "https://api.themoviedb.org/3/search/person?api_key=6b93b25da5cdb9298216703c40a31832&language=en-US&query=\(searchText.replacingOccurrences(of: " ", with: "%20"))&include_adult=false&page="
-            defaultURL = searchURL
-           pageNo = 1
-            getData(pageNumber: 1, urlString: searchURL)
-            self.tableView.reloadData()
+            if (searchBar.text != nil){
+                  let searchURL = "https://api.themoviedb.org/3/search/person?api_key=6b93b25da5cdb9298216703c40a31832&language=en-US&query=\(searchText.replacingOccurrences(of: " ", with: "%20"))&include_adult=false&page="
+                  defaultURL = searchURL
+                  pageNo = 1
+                  getData(pageNumber: 1, urlString: searchURL)
+                  self.tableView.reloadData()
+            }else{
+                  self.tableView.reloadData()
+            }
             
             
       }
       
+      
+      
+//      https://api.themoviedb.org/3/search/person?api_key=6b93b25da5cdb9298216703c40a31832&language=en-US&query=Dd&include_adult=false&page=1
+//
+//
       override func viewDidLoad() {
             super.viewDidLoad()
-            searchField.delegate = self
-            
+            searchBar.delegate = self
+         
+            searchBar.showsCancelButton = true
             
             activity.isHidden = true
             
@@ -79,19 +123,12 @@ class TableViewController: UITableViewController, UISearchBarDelegate {
             // Code to refresh table view
             
             print("refresh is choosen")
-            
+            searchBar.resignFirstResponder()
+            searchBar.endEditing(true)
             self.pageNo = 1
          //   self.isDataLoading = false
             getData(pageNumber: pageNo, urlString: defaultURL)
             self.refreshControl?.endRefreshing()
-            
-      }
-      
-      
-      
-      
-      
-      func getImage(str : String , indx : IndexPath){
             
       }
       
@@ -122,7 +159,7 @@ class TableViewController: UITableViewController, UISearchBarDelegate {
                                           
                                           self.apiTotalPages = dictionary?["total_pages"] as? Int
                                           
-//                                          self.pageNo = dictionary?["page"] as? Int ?? 1
+                                          //                                          self.pageNo = dictionary?["page"] as? Int ?? 1
                                           for result in results{
                                                 
                                                 let person = Person()
@@ -133,8 +170,8 @@ class TableViewController: UITableViewController, UISearchBarDelegate {
                                                 self.arrayOfPersons.append(person)
                                           }
                                           self.pageNo += 1
-                                      //    self.isDataLoading = false
-                                          print ("the global page no is\(self.pageNo-1) totalpages is \(self.apiTotalPages)")
+                                          //    self.isDataLoading = false
+                                          print ("the global page no is\(self.pageNo-1) totalpages is \(self.apiTotalPages ?? 0)")
                                           
                                           print("\(self.arrayOfPersons.count)")
                                           self.tableView.reloadData()
@@ -142,10 +179,7 @@ class TableViewController: UITableViewController, UISearchBarDelegate {
                                     }
                               }
                         }
-                        //                        else {
-                        //                              self.getData(pageNumber: 1 , urlString: self.peopleURL)
-                        //                        }
-                        
+                    
                   } catch {
                         print("JSON error: \(error.localizedDescription)")
                   }
@@ -196,18 +230,43 @@ class TableViewController: UITableViewController, UISearchBarDelegate {
             self.uiLable = cell.viewWithTag(2) as? UILabel
             self.uiImageview = cell.viewWithTag(1) as? UIImageView
             
-            if let  temp = arrayOfPersons[indexPath.row].path  {
+            if arrayOfPersons[indexPath.row].path != nil  {
                   
                   let urlString = "https://image.tmdb.org/t/p/w500/"+arrayOfPersons[indexPath.row].path!
-                  let url = URL(string: urlString)
+                  //                  let url = URL(string: urlString)
                   uiImageview.imageFromUrl(urlString: urlString)
             }else{
                   self.uiImageview.image = UIImage(named: "avatar")
             }
                   self.uiLable.text = self.arrayOfPersons[indexPath.row].name
-            
-
-            
+//
+//            if(arrayOfPersons[indexPath.row].path != nil){
+//
+//                  let urlString = "https://image.tmdb.org/t/p/w500/"+arrayOfPersons[indexPath.row].path!
+//                  let url = URL(string: urlString)
+//
+//                  if (url != nil){
+//
+//                        self.uiLable.text = self.arrayOfPersons[indexPath.row].name
+//                        uiImageview.imageFromUrl(urlString: urlString)
+//
+//
+//                        //                        setImage(from: url!, indx: indexPath)
+//                        //                        DispatchQueue.global().async {
+//                        //                              let data = try? Data(contentsOf: url!)
+//                        //                              DispatchQueue.main.async {
+//                        //
+//                        //                                    let cell = self.tableView.cellForRow(at: indexPath)
+//                        //                                    self.uiImageview = cell?.viewWithTag(1) as? UIImageView
+//                        //                                    self.uiLable = cell?.viewWithTag(2) as? UILabel
+//                        //                                    if(data != nil){
+//                        //                                          self.uiImageview.image = UIImage(data: data!)
+//                        //                                          self.uiLable.text = self.arrayOfPersons[indexPath.row].name
+//                        //                                    }
+//                        //
+//                        //                              }
+//                  }
+//            }
             return cell
             
       }
@@ -229,7 +288,7 @@ class TableViewController: UITableViewController, UISearchBarDelegate {
             
             // check if the last element
           
-            
+
             
       }
       
