8d2693d2ac8022cc61857e399613268a27f621e9
samira273 <samiramarassy@gmail.com>
2019-09-10 18:44:05 +0200

    10 Sept.

---
+++ IbtikarProject/Model/DataFetchModel.swift
@@ -0,0 +1,16 @@
+//
+//  DataFetchModel.swift
+//  IbtikarProject
+//
+//  Created by user on 9/10/19.
+//  Copyright © 2019 esraa mohamed. All rights reserved.
+//
+
+import Foundation
+class DataFetchModel : NSObject {
+    var arrayOfPersons : [Person] = []
+    
+    func loadDataOf(link url : String, forPageNO pageNo : Int){
+        
+    }
+}
